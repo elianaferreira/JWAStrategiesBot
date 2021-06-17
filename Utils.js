9ec5891bf087f2cb7321e2513d938fcc518eea51
Eliana Ferreira <elianaef817@gmail.com>
2021-06-17 09:14:51 -0400

    added new boss

--- Utils.js
+++ Utils.js
@@ -1,4 +1,4 @@
-const dinoNames = ["Sinoceratops", "Indominus Rex", "Grypolyth", "Gorgotrebax", "Woolly Mammoth", "Mammotherium", "Stygidaryx", "Hadros Lux", "Carnotaurus", "Blue", "Erlikogamma", "Pyrritator", "Indoraptor", "Mortem Rex", "Tyrannosaurus Rex", "Megalosuchus", "Smilonemys", "Ceramagnus", "Meiolania", "Refrenantem"]
+const dinoNames = ["Sinoceratops", "Indominus Rex", "Grypolyth", "Gorgotrebax", "Woolly Mammoth", "Mammotherium", "Stygidaryx", "Hadros Lux", "Carnotaurus", "Blue", "Erlikogamma", "Pyrritator", "Indoraptor", "Mortem Rex", "Tyrannosaurus Rex", "Scorpius Rex", "Smilonemys", "Ceramagnus", "Meiolania", "Refrenantem"]
 
 
 module.exports.isDayMessage = function (message) {
@@ -61,7 +61,7 @@ module.exports.thursdayKeyboard = function () {
   return {
     "parse_mode": "Markdown",
     "reply_markup": {
-      "keyboard": [["Tyrannosaurus Rex", "Megalosuchus"], ["Smilonemys", "Ceramagnus"], cancelArrayItem],
+      "keyboard": [["Tyrannosaurus Rex", "Scorpius Rex"], ["Smilonemys", "Ceramagnus"], cancelArrayItem],
       "resize_keyboard": true,
       "one_time_keyboard": true
     }
