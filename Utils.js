3bc74b02981f386af9483ec3084a21a7e6077ddd
Eliana Ferreira <elianaef817@gmail.com>
2021-10-07 09:28:57 -0300

    added new boss

--- Utils.js
+++ Utils.js
@@ -1,4 +1,4 @@
-const dinoNames = ["Postimetrodon", "Indominus Rex", "Andrewtops", "Gorgotrebax", "Woolly Mammoth", "Glyptoceras", "Stygidaryx", "Hadros Lux", "Carnotaurus", "Blue", "Megalotops", "Rinchicyon", "Indoraptor", "Mortem Rex", "Tyrannosaurus Rex", "Scorpius Rex", "Smilonemys", "Ceramagnus", "Meiolania", "Refrenantem", "Bajadasaurus", "Haast Maximus", "Hydra Boa", "Megalonyx"]
+const dinoNames = ["Postimetrodon", "Indominus Rex", "Andrewtops", "Gorgotrebax", "Woolly Mammoth", "Glyptoceras", "Stygidaryx", "Hadros Lux", "Carnotaurus", "Blue", "Megalotops", "Rinchicyon", "Indoraptor", "Mortem Rex", "Tyrannosaurus Rex", "Scorpius Rex", "Phorurex", "Ceramagnus", "Meiolania", "Refrenantem", "Bajadasaurus", "Haast Maximus", "Hydra Boa", "Megalonyx"]
 
 
 module.exports.isDayMessage = function (message) {
@@ -63,7 +63,7 @@ module.exports.thursdayKeyboard = function () {
   return {
     "parse_mode": "Markdown",
     "reply_markup": {
-      "keyboard": [["Tyrannosaurus Rex", "Scorpius Rex"], ["Smilonemys", "Megalonyx"], ["Ceramagnus"], cancelArrayItem],
+      "keyboard": [["Tyrannosaurus Rex", "Scorpius Rex"], ["Phorurex", "Megalonyx"], ["Ceramagnus"], cancelArrayItem],
       "resize_keyboard": true,
       "one_time_keyboard": true
     }
